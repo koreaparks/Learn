df3de9056c01d38db6f28a08f7a17d184a5c63ae
Koreaparks <koreaparks@gmail.com>
2023-04-09 17:24:21 +0900

    Create s2.kt

---
+++ etc/s2.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(str: String): String {
+        val moeum = arrayOf("a", "e", "i", "o", "u")
+        val mo: MutableList<Pair<Int, String>> = mutableListOf()
+        val strList = str.chunked(1).toMutableList()
+
+        strList.forEachIndexed { i, it ->
+            if (moeum.contains(it.lowercase())) {
+                mo += i to it
+            }
+        }
+
+        var lastIndex = mo.count() - 1
+        mo.forEachIndexed { i, it ->
+            strList[it.first] = mo[lastIndex].second
+            lastIndex--
+        }
+
+        return strList.joinToString("")
+    }
+}
