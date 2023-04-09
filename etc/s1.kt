b4b97196031b54a355ddb586a5bfae48f748f429
Koreaparks <koreaparks@gmail.com>
2023-04-09 17:23:43 +0900

    Add files via upload

---
+++ etc/s1.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(numbers: IntArray, goal: Int): IntArray {
+        val split = numbers.mapIndexed { index, it -> index to it}
+                            .filter { it.second < goal }
+
+        for (i in 0..split.count()) {
+            //기준점
+            val now = split[i]
+
+            split.forEachIndexed { index, it ->
+                if (index == i) return@forEachIndexed
+
+                if (now.second + it.second == goal) {
+                    return intArrayOf(now.first, it.first)
+                }
+            }
+        }
+
+        return intArrayOf()
+    }
+}
